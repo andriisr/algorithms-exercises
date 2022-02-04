effcc1c230ba7536566c7a7976edf33874c52a47
Andrew Rybakov <arybakov@hubspot.com>
2022-02-03 20:56:30 -0500

    radix sort

--- specs/radix-sort/radix-sort.test.js
+++ specs/radix-sort/radix-sort.test.js
@@ -3,66 +3,85 @@
   Implement a radix sort in a function called radixSort.
 
   You'll probably need several functions
-  
+
   You can implement it using a binary or decimal based bucketing but I'd recommend the decimal based buckets because
   it ends up being a lot more simple to implement.
 
 */
 
+const makeBuckets = () => {
+  const buckets = [
+    [], // 0s
+    [], // 1s
+    [], // 2s
+    [], // 3s
+    [], // 4s
+    [], // 5s
+    [], // 6s
+    [], // 7s
+    [], // 8s
+    [], // 9s
+  ];
+
+  const putInABucket = (num, indexFromEnd) => {
+    const numString = num.toString();
+    if (indexFromEnd > numString.length) {
+      buckets[0].push(num);
+    } else {
+      const digit = Number(numString.slice(-indexFromEnd)[0]);
+      buckets[digit].push(num);
+    }
+  };
+
+  const dequeueBuckets = () => {
+    return buckets.flat();
+  };
+
+  return {
+    putInABucket,
+    dequeueBuckets,
+  };
+};
+
 function radixSort(array) {
-  // code goes here
+  // const startTimestamp = Date.now();
+
+  // find max number length
+  let maxLength = 0;
+  for (let num of array) {
+    const currLength = num.toString().length;
+    maxLength = currLength > maxLength ? currLength : maxLength;
+  }
+
+  // sort
+  let result = array;
+  for (let i = 1; i <= maxLength; i++) {
+    const { putInABucket, dequeueBuckets } = makeBuckets(maxLength);
+    for (let numString of result) {
+      putInABucket(numString, i);
+    }
+    result = dequeueBuckets();
+  }
+
+  // console.log(Date.now() - startTimestamp);
+  return result;
 }
 
-// unit tests
-// do not modify the below code
-describe.skip("radix sort", function () {
+// const nums = new Array(1000000)
+//   .fill()
+//   .map(() => Math.floor(Math.random() * 500000));
+// radixSort(nums);
+
+describe("radix sort", function () {
   it("should sort correctly", () => {
     const nums = [
-      20,
-      51,
-      3,
-      801,
-      415,
-      62,
-      4,
-      17,
-      19,
-      11,
-      1,
-      100,
-      1244,
-      104,
-      944,
-      854,
-      34,
-      3000,
-      3001,
-      1200,
-      633
+      20, 51, 3, 801, 415, 62, 4, 17, 19, 11, 1, 100, 1244, 104, 944, 854, 34,
+      3000, 3001, 1200, 633,
     ];
     const ans = radixSort(nums);
     expect(ans).toEqual([
-      1,
-      3,
-      4,
-      11,
-      17,
-      19,
-      20,
-      34,
-      51,
-      62,
-      100,
-      104,
-      415,
-      633,
-      801,
-      854,
-      944,
-      1200,
-      1244,
-      3000,
-      3001
+      1, 3, 4, 11, 17, 19, 20, 34, 51, 62, 100, 104, 415, 633, 801, 854, 944,
+      1200, 1244, 3000, 3001,
     ]);
   });
   it("should sort 99 random numbers correctly", () => {
@@ -71,6 +90,7 @@ describe.skip("radix sort", function () {
       .fill()
       .map(() => Math.floor(Math.random() * 500000));
     const ans = radixSort(nums);
-    expect(ans).toEqual(nums.sort());
+
+    expect(ans).toEqual(nums.sort((a, b) => a - b));
   });
 });
